530912d2b5a407187591c884d07a6257bb6541fc
Morgan Stanley <morganpstanley@gmail.com>
2019-07-02 19:29:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,10 +34,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array.pop
+  array.first
 
 end
 
 def retrieve_last_element_from_array(array)
+  array.last
 
 end
